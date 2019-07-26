d1adf69fa76f15ac845343deb6eeb7f091082e17
9elight <53308315+9elight@users.noreply.github.com>
2019-07-26 13:09:49 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int []numbers = new int[5];
+        numbers[0] = 11;
+        numbers[3] = 34;
+        numbers[1] = 12;
+        System.out.println(numbers [4]);
+
+        String names [] = new String[33];
+        System.out.println(names [12]);
+
+        double [] digitsArray = {1.2, 45.6, 7.8, 225.5};
+
+        System.out.println(digitsArray[3]);
+
+        int [][]  array = {{1,2}, {3,4}, {5,6}, {7,8}};
+        System.out.println(array [0] [1]);
+
+    }
+}
